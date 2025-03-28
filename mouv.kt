fed219deda0c129c5129275b4e52d004cd05b83d
Sotoca--Ferrata Julien <126296180+Managrame@users.noreply.github.com>
2025-03-28 16:16:00 +0100

    Update mouv.kt

--- mouv.kt
+++ mouv.kt
@@ -21,6 +21,29 @@ class Mouv(var montant:Float,var sign:Int,var cat:String, var sender_receiver:St
         return sender_receiver
     }
 
+    fun comparedate( l1:List<Int>, l2:List<Int>):Int{
+        
+        if(l1[2].compareTo(l2[2])<0){
+            return -1
+        }
+        else if(l1[2].compareTo(l2[2])>0){return 1}
+        else{
+            if(l1[1].compareTo(l2[1])<0){
+                return -1
+            }
+            else if(l1[1].compareTo(l2[1])>0){return 1}
+            else{
+                if(l1[0].compareTo(l2[0])<0){
+                    return -1
+                }
+                else if(l1[0].compareTo(l2[0])>0){return 1}
+                else{
+                    return 0
+                }
+            }
+        }
+    }
+
     
 
 }
