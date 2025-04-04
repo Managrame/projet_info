bcf5a3f85c05afa8b8c9130a610304aec1cb1afe
julien_sotoca_ferrata <jx1001.29@gmail.com>
2025-04-04 13:49:05 +0200

    account

--- Account.kt
+++ Account.kt
@@ -1,9 +1,12 @@
+import kotlin.collections.mutableListOf
+import kotlin.collections.mutableListOf
 class Account(val accountNumber: Int) {
     var balance = 0.0
-    var history: mutableListOf<Mouv>() = mutableListOf()
-    fun deposit(amount: Double) {
+    var history: MutableList<Mouv> = mutableListOf()
+   
+    fun deposit(amount: Float,cat:String,sr:String,date:List<Int>) {
         balance += amount
-        history.add(Mouv(amount, "deposit"))
+        history.add(Mouv(amount, amount.compareTo(0),cat,sr,date))
     }
 
 }
